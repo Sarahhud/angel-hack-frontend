ac472419c105defbcf2cc06fe03b3b171555320c
dtruong11 <dtruong11@users.noreply.github.com>
2018-07-22 07:31:50 -0700

    Render functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,45 @@
+const renderHome = require('./render/render-home')
+const renderVideo = require('./render/render-video')
+
+renderHome()
+
 const allAcceptButtons = document.querySelectorAll('.buttons-acc')
 const allDeclineButtons = document.querySelectorAll('.buttons-decline')
+const AgoraRTC = require('agora-rtc-sdk')
+const appId = require('./appID')
 
 const acceptBtns = Array.from(allAcceptButtons)
 const declineBtns = Array.from(allDeclineButtons)
 
+// add event listener to accept buttons
 acceptBtns.map(el => {
-    el.addEventListener('click', function () {
-        console.log('Hello')
-    })
+  el.addEventListener('click', join)
+})
+// add event listeners to decline buttons
+declineBtns.map(el => {
+  el.addEventListener('click', function() {
+    // console.log(AgoraRTC)
+  })
 })
 
-declineBtns.map(el => {
-    el.addEventListener('click', function () {
-        console.log('Hello')
-    })
-})
+if (!AgoraRTC.checkSystemRequirements()) {
+  alert("Your browser does not support WebRTC!");
+}
+
+let client, localStream, camera, microphone;
+
+function join() {
+	const channel_key = null;
+	const channelVal = 1000;
+  client = AgoraRTC.createClient({
+    mode: 'interop' //What is this mode?
+  });
+  console.log(client)
+  client.init(appId, function() {
+    console.log("AgoraRTC client initialized");
+		client.join(channel_key, channelVal, null, function(uid){
+			console.log("User " + uid + " join channel successfully");
+			
+		})
+  })
+}
--- scripts/render/render-home.js
+++ scripts/render/render-home.js
@@ -1,16 +1,6 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>Doctor Profile</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/main.css">
-</head>
-
-<body>
-    <!-- nav bar
+function homeTemplate () {
+    return `
+    <!-- nav bar -->
     <div class="topnav">
         <a class="float-left" href="#home">GEMS</a>
         <form class="float-left form-inline my-5 my-lg-0">
@@ -22,6 +12,7 @@
         <a class="float-right mr-2" href="#about">About</a>
     </div>
 
+    <!-- urgent request -->
     <div class='container mt-3'>
         <div>
             <img src="./assets/Icons/Urgent-Request.png" alt="">
@@ -108,6 +99,7 @@
         </div>
     </div>
 
+    <!-- Emergency Event -->
     <div class="container mt-3 d-flex flex-row bd-highlight mb-5">
         <div class="col">
             <div class="card" style="width: 18rem;">
@@ -157,15 +149,11 @@
             </div>
         </div>
     </div>
-     -->
+    `
+}
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
-        crossorigin="anonymous"></script>
-    <script type="text/javascript" src="/bundle.js"></script>
-</body>
+function renderHome () {
+    document.querySelector('body').innerHTML = homeTemplate() 
+}
 
-</html>
+module.exports = renderHome;
